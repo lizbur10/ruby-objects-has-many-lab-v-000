13966d22baa69dcc209418e5eee8e751a33c8769
Liz Burton <liz.burton147@gmail.com>
2017-12-02 13:58:49 +0000

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -5,28 +5,3 @@ class Post
     @name = name
   end
 end
-
-
-
-
-class Dog
-  def name=(dogs_name)
-    @this_edogs_name = dogs_name
-  end
-
-  def name
-    @this_dogs_name
-  end
-end
-fido = Dog.new
-fido.name ="Fido"
-
-puts fido.name
-
-snoopy = Dog.new
-snoopy.name = "Snoopy"
-puts snoopy.name
-
-lassie = Dog.new
-lassie.name = "Lassie"
-puts lassie.name

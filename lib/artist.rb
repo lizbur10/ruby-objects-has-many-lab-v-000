a3a5304e992c590c8433602be27519e363e8de0e
Liz Burton <liz.burton147@gmail.com>
2017-12-08 19:24:27 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,6 @@
 class Artist
   attr_reader :name, :songs, :artist
+  @@song_count = 0
 
   def initialize(name)
     @name = name
@@ -9,6 +10,7 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
@@ -17,6 +19,6 @@ class Artist
   end
 
   def self.song_count
-
+    @@song_count
   end
 end
